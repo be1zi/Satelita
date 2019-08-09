0ba5c2039e7092d54f1fc61f25351e90b529bf44
Konrad Bełzowski <konrad.belzowski@gmail.com>
2019-08-09 09:31:27 +0200

    Init

---
+++ Satelita/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Satelita
+//
+//  Created by Konrad Belzowski on 09/08/2019.
+//  Copyright © 2019 Konrad Belzowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
