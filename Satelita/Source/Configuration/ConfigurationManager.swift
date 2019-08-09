de1652dd4bb9dddd83895053538fa0682c8166ab
Konrad Bełzowski <konrad.belzowski@gmail.com>
2019-08-09 12:14:19 +0200

    project structure

---
+++ Satelita/Source/Configuration/ConfigurationManager.swift
@@ -0,0 +1,9 @@
+//
+//  ConfigurationManager.swift
+//  Satelita
+//
+//  Created by Konrad Belzowski on 09/08/2019.
+//  Copyright © 2019 Konrad Belzowski. All rights reserved.
+//
+
+import Foundation
