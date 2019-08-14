7bfb6ffdbe5228258d2b7ae920fdefeb0731980f
Konrad Bełzowski <konrad.belzowski@gmail.com>
2019-08-14 09:19:02 +0200

    add configuration manager

--- Satelita/Source/Configuration/ConfigurationManager.swift
+++ Satelita/Source/Configuration/ConfigurationManager.swift
@@ -7,3 +7,70 @@
 //
 
 import Foundation
+
+struct ConfigurationManager {
+    
+    //
+    // MARK: - Singleton
+    //
+    
+    static let sharedInstance = ConfigurationManager()
+    
+    //
+    // MARK: - Getters
+    //
+    
+    var serverAddress: String? {
+     return getProperty(name: "serverAddress", withRoot: "API")
+    }
+    
+    //
+    // MARK: - Helpers
+    //
+    
+    private func loadConfiguration() -> [String: Any]? {
+        
+        let fileURL = Bundle.main.url(forResource: "config", withExtension: "plist")
+        
+        guard let url = fileURL else {
+            return nil
+        }
+        
+        let fileData: Data
+        let dict: [String: Any]?
+        
+        do {
+            fileData = try Data.init(contentsOf: url)
+        } catch {
+            print(error)
+            return nil
+        }
+        
+        do {
+            dict = try PropertyListSerialization.propertyList(from: fileData, format: nil) as? [String: Any]
+        } catch {
+            print(error)
+            return nil
+        }
+        
+        return dict
+    }
+    
+    private func getProperty(name: String, withRoot root: String) -> String? {
+        let dict = loadConfiguration()
+        
+        guard let properties = dict else {
+            return nil
+        }
+        
+        guard let api = properties[root] as? [String: Any] else {
+            return nil
+        }
+        
+        guard let result = api[name] as? String else {
+            return nil
+        }
+        
+        return result
+    }
+}
